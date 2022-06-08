d385f742f3e9f9ff5d6155bb9ea5429ebccbf6c0
Oluwatuyi Segun <63879692+soluwatuyi@users.noreply.github.com>
2022-06-08 03:31:38 +0100

    Fixed cookies

--- refresh.php
+++ refresh.php
@@ -7,6 +7,8 @@
 	header("Access-Control-Max-Age: 0");
 	header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 	if(isset($_COOKIE["name"])) {
+		setcookie("name", $_COOKIE["name"], (time() + 1800), "/", $dom);
+		setcookie("username", $_COOKIE["username"], (time() + 1800), "/", $dom);
 		setcookie("lastID", $_COOKIE["lastID"], (time() + 1800), "/", $dom);
 		$id = intval($_GET["lastID"]);
 		$jsonData = chatClass::getRestChatLines($id);
